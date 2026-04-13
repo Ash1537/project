d8b0d0c8ab77d63bea9b59a9bf137a431ecda0fa
Ash <pyaephyo153007@gmail.com>
2026-04-13 14:29:17 +0800

    fix route test

--- api/[uid].js
+++ api/[uid].js
@@ -1,19 +1,3 @@
 export default function handler(req, res) {
-  const { uid } = req.query;
-
-  const name = uid || "User";
-
-  return res.status(200).send(`
-    <html>
-      <head>
-        <meta property="og:title" content="${name}" />
-        <meta property="og:description" content="KatCard Profile" />
-        <meta property="og:image" content="https://via.placeholder.com/1200x630.png?text=${name}" />
-        <meta property="og:url" content="https://${req.headers.host}/${uid}" />
-      </head>
-      <body>
-        Profile page for ${name}
-      </body>
-    </html>
-  `);
+  return res.status(200).send("HELLO FROM UID ROUTE");
 }
